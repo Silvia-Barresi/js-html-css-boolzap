29473e851d2b94d38aa9a40fbec182632d618619
Silvia <sylwia.barresi@gmail.com>
2020-04-07 22:57:08 +0200

    sent message

--- js/main.js
+++ js/main.js
@@ -1,1 +1,21 @@
-// Aggiunta di un messaggio: l’utente scrive un testo nella parte bassa e cliccando invia il testo viene aggiunto al thread sopra, come messaggio verde 
+// Aggiunta di un messaggio: l’utente scrive un testo nella parte bassa e cliccando invia il testo viene aggiunto al thread sopra, come messaggio verde
+
+
+$(document).ready(function(){
+
+ var text = $('.newtext');
+
+ var send = $('.fa-paper-plane');
+
+ send.click(
+   function () {
+     var usersend = $('#mytext').val();
+
+     text.append(usersend);
+     $('input').val("<span>" + userend + "<span>");
+   }
+
+ );
+
+
+});
