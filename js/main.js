089f2835628c5af0312a61e1b3f08d7307c5fa83
Silvia <sylwia.barresi@gmail.com>
2020-04-08 21:28:36 +0200

    ricerca contatti

--- js/main.js
+++ js/main.js
@@ -45,7 +45,7 @@ $(document).ready(function(){
       // ciclo tra i contatti
       $('.contact').each(function(){
         // salvo il valore del testo
-        var myFriend = $(this).find('.myfriend').text().toLowerCase();
+        var myFriend = $(this).find('h2').text().toLowerCase();
 
         // se il contatto esiste lo mostro e nascondo gli altri
         if(myFriend.includes(searchcontacts)){
@@ -69,24 +69,6 @@ $(document).ready(function(){
 
 
 
-
-
-
-
-
-
- // filtro contatti
-  //gestirte evento su tastiera (oppure su click di bottone di input ricerca)
-
-  // salvarmi input utente in campo del filtro (stringa1)
-
-  // selezionare tutti i blocchi di contatto e ciclare tra di essi (each())
-    //salvo in una var il valore del testo del nome nel contatto (stringa2)
-
-    // confronto per vedere se la stringa inserita nell'input è inclusa nel nome del contatto
-      //stringa2.includes(stringa1)
-      //se l'occorenza è stata trovata lascio il blocco di contatto visibile
-      // altrimenti lo rendo non visibile (this)
 
 
 });
