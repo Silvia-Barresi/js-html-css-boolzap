7212fe2be8c5624afccf9c85b255e5a09ff97cea
Silvia <sylwia.barresi@gmail.com>
2020-04-13 13:51:12 +0200

    Ad ognuno la sua chat

--- js/main.js
+++ js/main.js
@@ -1,112 +1,160 @@
-// Aggiunta di un messaggio: l’utente scrive un testo nella parte bassa e cliccando invia il testo viene aggiunto al thread sopra, come messaggio verde
 
 
 $(document).ready(function(){
 
+
+// variabili milestone 1
  var text = $('.mainchat');
  var send = $('.fa-paper-plane');
- // var search =$('.fa-search')
 
 
- send.click(
-   function () {
+// invio un messaggio-----------------------------------------------------------
+   send.click(
+     function () {
 
-     // valore input
-     var userSend = $('#mytext').val();
-     // stampo il messaggio
-     text.append('<div class="message greenmessage"><span>' + userSend + '</span><i class="fas fa-chevron-down"></i></div>');
-     // input vuoto
-     $('input').val('');
-     // dopo un secondo arriva la risposta automatica
-     setTimeout(function(){
-       text.append('<div class="message whitemessage"><span>' + 'ok' + '</span><i class="fas fa-chevron-down"></i></div>');
-     }, 1000);
+       // valore input
+       var userSend = $('#mytext').val();
+       // stampo il messaggio
+       text.append('<div class="message greenmessage"><span>' + userSend + '</span><i class="fas fa-chevron-down"></i><span class="hour">15:40</span><div class="deletemenu"><ul><li>Info messaggio</li><li class="delete">Cancella messaggio</li></ul></div></div>');
+       // input vuoto
+       $('input').val('');
+       // dopo un secondo arriva la risposta automatica
+       setTimeout(function(){
+        text.append('<div class="message whitemessage"><span>' + 'ok' + '</span><i class="fas fa-chevron-down"></i><span class="hour">15:40</span><div class="deletemenu"><ul><li>Info messaggio</li><li class="delete">Cancella messaggio</li></ul></div></div>');
+       }, 1000);
 
-   }
+     }
 
- );
+   );
 
-    // cliccando enter, invio il messaggio
+// cliccando enter, invio il messaggio------------------------------------------
 
-      $('.container').keypress(function(enter) {
-      if ( enter.which == 13 ) {
-      send.click();
-      }
-      });
-
-
-  // ricerca tra i contatti con click su search
+    $('.container').keypress(function(enter) {
+    if ( enter.which == 13 ) {
+    send.click();
+    }
+    });
 
 
-      $('#friendinput').keyup(function(){
+// ricerca tra i contatti con click su search-----------------------------------
 
-        var searchcontacts = $(this).val().toLowerCase();
 
-          $('.contact').each(function(){
-
-          var myFriend = $(this).find('h2').text().toLowerCase();
-
-          if(myFriend.includes(searchcontacts)){
-              $(this).show();
-          }else{
-             $(this).hide();
-           }
-         });
-
+    $('#friendinput').keyup(function(){
+      // salvo l'input
+      var searchcontacts = $(this).val().toLowerCase();
+       // ciclo tra i contatti
+        $('.contact').each(function(){
+      // salvo il valore del testo
+        var myFriend = $(this).find('h2').text().toLowerCase();
+      // se il contatto esiste lo mostro e nascondo gli altri
+        if(myFriend.includes(searchcontacts)){
+            $(this).show();
+        }else{
+           $(this).hide();
+         }
        });
 
-  // search.click(
-  //   function(){
-  //     // salvo l'input
-  //     var searchcontacts = $('#friendinput').val().toLowerCase();
-  //
-  //     // ciclo tra i contatti
-  //     $('.contact').each(function(){
-  //       // salvo il valore del testo
-  //       var myFriend = $(this).find('h2').text().toLowerCase();
-  //
-  //       // se il contatto esiste lo mostro e nascondo gli altri
-  //       if(myFriend.includes(searchcontacts)){
-  //         $(this).show();
-  //       }else{
-  //         $(this).hide();
-  //   });
-  //
-  //   }
-  // );
-
-      // cancello il messaggio dal menu a tendina
-
-        // nascondo il menu a tendina
-        $('.deletemenu').hide();
-
-        // al click spunta il menù e al click lo nascondo nuovamente
-
-        $('.message').on('click','.fa-chevron-down',
-            function(){
-
-              var menuShow = $(this).siblings('.deletemenu');
-              // se fratello menu è nascosto mostralo
-              if (menuShow.is(':hidden')){
-                menuShow.show();
-              // altrimenti nascondilo
-              }else{
-                menuShow.hide();
-              }
-
-            });
-
-        // cancello il messaggio cliccando sull'opzione dal menu a tendina
-        $('.mainchat').on('click', '.delete',
-            function(){
-        // rimuovi tutta la famiglia del menu a tendina
-              $(this).parents('.message').remove();
-            });
+     });
 
 
+          // ALTERNATIVA al keyup con click sull'icona search
+
+          // var search = $(.fa-search)
+
+          // search.click(
+          //   function(){
+          //     // salvo l'input
+          //     var searchcontacts = $('#friendinput').val().toLowerCase();
+          //
+          //     // ciclo tra i contatti
+          //     $('.contact').each(function(){
+          //       // salvo il valore del testo
+          //       var myFriend = $(this).find('h2').text().toLowerCase();
+          //
+          //       // se il contatto esiste lo mostro e nascondo gli altri
+          //       if(myFriend.includes(searchcontacts)){
+          //         $(this).show();
+          //       }else{
+          //         $(this).hide();
+          //   });
+          //
+          //   }
+          // );
+
+
+// cancello il messaggio dal menu a tendina-------------------------------------
+
+    // al click spunta il menù e al click lo nascondo nuovamente
+    $('.message').on('click','.fa-chevron-down',
+        function(){
+
+          $(this).siblings('.deletemenu').toggle();
+
+              // ALTERNATIVA AL TOGGLE
+
+              // var menuShow = $(this).siblings('.deletemenu');
+              // // se fratello menu è nascosto mostralo
+              // if (menuShow.is(':hidden')){
+              //   menuShow.show();
+              // // altrimenti nascondilo
+              // }else{
+              //   menuShow.hide();
+              // }
+
+
+    });
+
+// cancello il messaggio cliccando sull'opzione dal menu a tendina--------------
+    $('.mainchat').on('click', '.delete',
+        function(){
+    // rimuovi tutta la famiglia del menu a tendina
+          $(this).parents('.message').remove();
+    });
 
 
 
+
+
+// Click sul contatto mostra la conversazione del contatto cliccato-------------
+// è possibile inserire nuovi messaggi per ogni conversazione-------------------
+
+    $('.contact').click(
+      function(){
+
+        // rimuovo la classe active dal contatto e la aggiungo al contatto su cui clicco
+        $('.contact').removeClass('active');
+        $(this).addClass('active');
+
+        // salvo la variabile nome contatto sull'header della hat per sostituirla con il nome del contatto su cui clicco
+        var contactName = $(this).find('.myfriend').text();
+        $('.imchattingwith h2').text(contactName);
+
+        // come prima ma con l'immagine
+        var contactImg = $(this).find('img').attr('src');
+        $('.contactright a img').attr('src', contactImg);
+
+        // salvo la conversazione che mi interessa
+        var chat = $(this).data('chat');
+
+        // nascondo l'active dalle chat che non mi interessano e la aggiungo a quella su cui clicco
+        $('.mainchat').removeClass('active').addClass('ghost');
+        $('.mainchat').eq(chat).removeClass('ghost').addClass('active');
+
+              // ALTERNATIVA MAINCHAT DATA
+
+              // salvo la conversazione che mi interessa attraverso l'attributo data
+              // var conversazione = $(this).attr('data-chat');
+
+
+              // salvo la sezione che contiene la conversazione che mi interessa
+              // var mainChat = $('.mainchat[data-chat="' + conversazione + '"]');
+
+              // nascondo l'active dalle chat che non mi interessano e la aggiungo a quella su cui clicco
+              // $('.mainchat').removeClass('active');
+              // mainChat.addClass('active');
+
+
+    });
 
 
 });
