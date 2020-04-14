cc8b58cf3a0b09524a5189a14adf7a595c7f88ef
Silvia <sylwia.barresi@gmail.com>
2020-04-14 17:26:25 +0200

    aggiustamenti

--- js/main.js
+++ js/main.js
@@ -3,41 +3,62 @@
 $(document).ready(function(){
 
 
-// variabili milestone 1
- var text = $('.mainchat');
- var send = $('.fa-paper-plane');
+// variabili--------------------------------------------------------------------
+   var mainChat = $('.mainchat');
+   var send = $('.fa-paper-plane');
+   var contact = $('.contact');
 
- var today = new Date();
- var time = today.getHours() + ':' + today.getMinutes();
+// creo la variabile per l'ora--------------------------------------------------
+
+   var today = new Date();
+   var time = zero(today.getHours()) + ':' + zero(today.getMinutes());
+
+   function zero(number){
+     if (number < 10){
+       number = '0' + number;
+     }
+     return number
+   };
+
 
 // invio un messaggio-----------------------------------------------------------
-   send.click(
+
+    send.click(
      function () {
+
        // valore input
        var userSend = $('#mytext').val();
+       var activeChat = $('.mainchat.active');
 
-       // stampo il messaggio
-       $('.mainchat.active').append('<div class="message greenmessage"><span>' + userSend + '</span><i class="fas fa-chevron-down"></i><span class="hour">'+ time +'</span><div class="deletemenu"><ul><li>Info messaggio</li><li class="delete">Cancella messaggio</li></ul></div></div>');
-       // input vuoto
-       $('input').val('');
-       // dopo un secondo arriva la risposta automatica
-       setTimeout(function(){
-        $('.mainchat.active').append('<div class="message whitemessage"><span>' + 'ok' + '</span><i class="fas fa-chevron-down"></i><span class="hour">'+ time +'</span><div class="deletemenu"><ul><li>Info messaggio</li><li class="delete">Cancella messaggio</li></ul></div></div>');
-       }, 1000);
+       if (userSend.length > 0){
 
-       // inserisci l'orario dell'ultima visualizzazione e il messaggio sta scrivendo..
-       // $('.imchattingwith > p').text('Sta scrivendo...');
-
-    });
+         // stampo il messaggio
+         activeChat.append('<div class="message greenmessage"><span>' + userSend + '</span><i class="fas fa-chevron-down"></i><span class="hour">'+ time +'</span><div class="deletemenu"><ul><li>Info messaggio</li><li class="delete">Cancella messaggio</li></ul></div></div>');
+         // input vuoto
+         $('input').val('');
+         // dopo un secondo arriva la risposta automatica
+         setTimeout(function(){
+          activeChat.append('<div class="message whitemessage"><span>' + 'ok' + '</span><i class="fas fa-chevron-down"></i><span class="hour">'+ time +'</span><div class="deletemenu"><ul><li>Info messaggio</li><li class="delete">Cancella messaggio</li></ul></div></div>');
+         }, 1000);
 
 
+         // modifico l'orario di visualizzazione dei contatti
+          $('.imchattingwith > p span.hour').text(time);
+          $('.contact.active > span').text(time);
+
+        // la chat scrolla verso l'ultimo messaggio inviato
+          activeChat.scrollTop(activeChat.prop("scrollHeight"));
+
+        };
+
+     });
 
 // cliccando enter, invio il messaggio------------------------------------------
 
     $('.container').keypress(function(enter) {
-    if ( enter.which == 13 ) {
-    send.click();
-    }
+      if ( enter.which == 13 ) {
+      send.click();
+      }
     });
 
 
@@ -46,9 +67,9 @@ $(document).ready(function(){
 
     $('#friendinput').keyup(function(){
       // salvo l'input
-      var searchcontacts = $(this).val().toLowerCase();
-       // ciclo tra i contatti
-        $('.contact').each(function(){
+        var searchcontacts = $(this).val().toLowerCase();
+      // ciclo tra i contatti
+        contact.each(function(){
       // salvo il valore del testo
         var myFriend = $(this).find('h2').text().toLowerCase();
       // se il contatto esiste lo mostro e nascondo gli altri
@@ -90,7 +111,7 @@ $(document).ready(function(){
 // cancello il messaggio dal menu a tendina-------------------------------------
 
     // al click spunta il menù e al click lo nascondo nuovamente
-    $('.message').on('click','.fa-chevron-down',
+    mainChat.on('click','.fa-chevron-down',
         function(){
 
           $(this).siblings('.deletemenu').toggle('active');
@@ -105,12 +126,10 @@ $(document).ready(function(){
               // }else{
               //   menuShow.hide();
               // }
-
-
     });
 
 // cancello il messaggio cliccando sull'opzione dal menu a tendina--------------
-    $('.mainchat').on('click', '.delete',
+    mainChat.on('click', '.delete',
         function(){
     // rimuovi tutta la famiglia del menu a tendina
           $(this).parents('.message').remove();
@@ -123,11 +142,11 @@ $(document).ready(function(){
 // Click sul contatto mostra la conversazione del contatto cliccato-------------
 // è possibile inserire nuovi messaggi per ogni conversazione-------------------
 
-    $('.contact').click(
+    contact.click(
       function(){
 
         // rimuovo la classe active dal contatto e la aggiungo al contatto su cui clicco
-        $('.contact').removeClass('active');
+        contact.removeClass('active');
         $(this).addClass('active');
 
         // salvo la variabile nome contatto sull'header della hat per sostituirla con il nome del contatto su cui clicco
@@ -142,10 +161,10 @@ $(document).ready(function(){
         var chat = $(this).data('chat');
 
         // nascondo l'active dalle chat che non mi interessano e la aggiungo a quella su cui clicco
-        $('.mainchat').removeClass('active').addClass('ghost');
-        $('.mainchat').eq(chat).removeClass('ghost').addClass('active');
+        mainChat.removeClass('active').addClass('ghost');
+        mainChat.eq(chat).removeClass('ghost').addClass('active');
 
-        $('.contact.active > span').text(time);
+
 
               // ALTERNATIVA MAINCHAT DATA
 
@@ -161,7 +180,7 @@ $(document).ready(function(){
               // mainChat.addClass('active');
 
 
-    });
+        });
 
 
-});
+ });
